fdf7874efba3696c73c14fe012aa90c2b0a2b506
Van Nguyen <van.c.nguyen@ibm.com>
2018-07-29 11:41:36 -0400

    Initial commit

---
+++ catalog-service/src/main/java/com/sivalabs/catalogservice/CatalogserviceApplication.java
@@ -0,0 +1,12 @@
+package com.sivalabs.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogserviceApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(CatalogserviceApplication.class, args);
+   }
+}
